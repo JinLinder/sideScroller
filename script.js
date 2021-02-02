4b1ec28fb9e10e3dff9c7fdcbf5e305370f2f53c
JinLinder <jinlinder@hotmail.com>
2021-02-02 15:12:52 +0100

    moving enemies

--- script.js
+++ script.js
@@ -29,3 +29,31 @@ window.addEventListener("keydown", function (evt) {
 });
 
 //------------------ MOVING ENEMIES ------------------//
+
+let counter= 0;
+let right = 0
+let enemies = setInterval(function (){
+    counter ++;
+    right += 10;
+    console.log (counter);
+    if (counter==3) {
+      clearInterval(enemies)
+      console.log("Hej", right);
+      counter=0;
+      let newEnemy = document.createElement("div");
+      newEnemy.setAttribute("class", "enemy");
+      wrapper.appendChild(newEnemy);
+      newEnemy.style.position = "absolute";
+      newEnemy.style.right += right + "px";
+      newEnemy.style.top = Math.random()*100 + "vh";
+      console.log("remove", newEnemy.style.right)
+        if(newEnemy.style.left<0+"px") {
+          
+          //newEnemy.remove();
+          
+        }
+  }
+}, 1000);
+
+
+
