90ea5f5bdcb448e6e797217f64741ed27c468800
JinLinder <jinlinder@hotmail.com>
2021-02-03 15:10:47 +0100

    creat and move enemies

--- script.js
+++ script.js
@@ -47,61 +47,42 @@ function moveEnemy () {
 };
 moveEnemy();
 
+//------------------ CREAT NEW ENEMIES AND MOVE THEM ------------------//
 
-  function moveEnemies (){
-    let newCount=0;
-    let newEnemyInterval = setInterval(newFrame, 500);
-    function newFrame () {
-      if (newCount==950) { 
-        clearInterval(newEnemyInterval);
-        console.log("clear newCount");
-      }
-      else if (newCount<=10) {
-        newCount ++;
-        let newEnemy = document.createElement("div");
-        wrapper.appendChild(newEnemy);
-        console.log("creat new div")
-        newEnemy.style.position = "absolute";
-        newEnemy.style.width= 50+"px";
-        newEnemy.style.height= 50+"px";
-        newEnemy.style.borderRadius = 50 + "%";
-        newEnemy.style.backgroundColor = "red";
-        newEnemy.style.right = newCount + "px";
-        newEnemy.style.top = Math.random()*500 + "px";
+function moveEnemies (){
+  let newCount=0;
+  let newEnemyInterval = setInterval(newFrame, 500);
+  function newFrame () {
+    newCount ++;
+    if (newCount==950) { 
+      clearInterval(newEnemyInterval);
+      console.log("clear newCount");
+    }
+    else if (newCount<12) {
+      let newEnemy = document.createElement("div");
+      wrapper.appendChild(newEnemy);
+      console.log("creat new div")
+      newEnemy.style.position = "absolute";
+      newEnemy.style.width= 20+"px";
+      newEnemy.style.height= 20+"px";
+      newEnemy.style.borderRadius = 50 + "%";
+      newEnemy.style.backgroundColor = "red";
+      newEnemy.style.top = Math.random()*500 + "px";
+    
+      let right=0;
+      let enemyToLeft= setInterval (newNFrame, 10);
+      function newNFrame () {
+        right ++;
+        if (right==950) {
+          clearInterval(enemyToLeft);
+        }
+        else {
+          newEnemy.style.right = right + "px";
+        } 
       }
     }
-  };
-  moveEnemies ();
-
-  
-  
-    
-
-
-// let counter= 0;
-// let right = 0
-// let enemies = setInterval(function (){
-//     counter ++;
-//     right += 10;
-//     console.log (counter);
-//     if (counter==3) {
-//       clearInterval(enemies)
-//       console.log("Hej", right);
-//       counter=0;
-//       let newEnemy = document.createElement("div");
-//       newEnemy.setAttribute("class", "enemy");
-//       wrapper.appendChild(newEnemy);
-//       newEnemy.style.position = "absolute";
-//       newEnemy.style.right += right + "px";
-//       newEnemy.style.top = Math.random()*100 + "vh";
-//       console.log("remove", newEnemy.style.right)
-//         if(newEnemy.style.left<0+"px") {
-          
-//           newEnemy.remove();
-          
-//         }
-//   }
-// }, 1000);
-
+  }
+};
+moveEnemies ();
 
 
